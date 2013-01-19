b1ce49cbde1d668f973c361ad4024e4bd5c24975
Dereckson <dereckson@espace-win.org>
2013-01-19 16:20:28 +0100

    Content: creating a _documents.xml for gadgets/
    Code belonging to <head> moved from <body> to the XML, for <head> inclusion.

--- gadgets/motd-variations.php
+++ gadgets/motd-variations.php
@@ -1,10 +1,3 @@
-     <!-- Extra fonts and layout -->
-     <link href='http://fonts.googleapis.com/css?family=OFL+Sorts+Mill+Goudy+TT:regular,italic' rel='stylesheet' type='text/css'>
-     <style type="text/css">
-       #content { font-family: "OFL Sorts Mill Goudy TT"; }
-       #content p { white-space: pre; margin: 0 0 4em 2em; font-size: 1.125em; }
-      </style>
-
       <!-- Actions -->
       <div id="action-icons"><a href="?" title="Refresh this page"><i class="general foundicon-refresh"></i></a></div>
 
@@ -13,5 +6,6 @@
 	$content = `/home/dereckson/public_html/variants.tcl`;
 	$pos1 = strpos($content, '<h2>');
 	$pos2 = strpos($content, '</body>');
+        $content = str_replace('<h2>', '<h3>', $content);
 	echo substr($content, $pos1, $pos2 - $pos1);
 ?>
