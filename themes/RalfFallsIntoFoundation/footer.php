59824126ea4830a878947dbaa5b0efcac96b32c8
Dereckson <dereckson@espace-win.org>
2013-01-19 16:06:24 +0100

    Pluton theme: RalfFallsIntoFoundation header and footer
    Generated from design.html, cutting after:
      <!-- Body -->
      <section id="content"><div class="row">
    
    Footer starts at:
      </div></section>
    
    So if you include a file with     <p>Hello world!</p> that will give:
    
    ...
    <!-- Body -->
    <section id="content"><div class="row">
        <p>Hello World!</p>
    </div></section>
    ...

---
+++ themes/RalfFallsIntoFoundation/footer.php
@@ -0,0 +1,59 @@
+  </div></section>
+
+  <!-- Footer -->
+  <footer><div class="row">
+  <div class="twelve columns"><hr />
+      <div class="row">
+        <div class="three columns">
+          <p>This site is a repository of tools.</p>
+          <p>This is also an experiment to create a site based on Pluton, Foundation, Hg <span class="ampersand">&</span> Nasqueron.</p>
+        </div>
+        <div class="three columns">
+        	<dl>
+			<dt>Gerrit</dt>
+			<dd>Activity feeds</dd>
+			<dd>RSS generator</dd>
+        	</dl>
+        </div>
+        <div class="three columns">
+        	<dl>
+			<dt>Network</dt>
+			<dd><a href="/network/mx.php">MX</a></dd>
+        	</dl>
+        </div>
+        <div class="three columns">
+        	<dl>
+			<dt>Gadgets</dt>
+			<dd><a href="/gadgets/motd-variations.php">MOTD in Jive <span class="ampersand">&</span> Valspeak</a></dd>
+        	</dl>
+        </div>
+      </div>
+  </div>
+  <div class="twelve columns"><hr />
+      <div class="row extrainfos">
+           <div class="six columns">
+               <p><i class="general foundicon-settings"></i> <strong>Options:</strong> <a href="javascript:SetUITonality('dark');">dark mode</a> | <a href="javascript:SetUITonality('light');">light mode</a></p>
+           </div>
+      </div>
+      <div class="row extrainfos">
+        <div class="seven columns">
+            <p><i class="general foundicon-globe"></i> <strong>Service author:</strong> <a href="http://www.dereckson.be/">Dereckson</a> | <strong>Powered by</strong> <a href="http://keruald.sf.net">Keruald/Pluton</a> <span class="ampersand">&</span> <a href="http://foundation.zurb.com/">Foundation</a>.</p>
+        </div>
+
+        <div class="five columns">
+            <ul class="link-list right">
+              <li><a href="http://www.dereckson.be/tools">DcK Area's tools</a></li>
+              <li><a href="http://www.espace-win.org/Tools">Espace Win's tools</a></li>
+              <li><a href="http://www.toolserver.org/~dereckson/">Toolserver</a></li>
+            </ul>
+        </div>
+
+      </div>
+  </div>
+  </div></footer>
+
+  <script src="/javascripts/foundation.min.js"></script>
+  <script src="/javascripts/jquery.cookie.js"></script>
+  <script src="/javascripts/app.js"></script>
+</body>
+</html>
---
+++ themes/RalfFallsIntoFoundation/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title><?= $document->title ?> - Tools</title>
+
+  <!-- Included CSS Files (Uncompressed) -->
+  <!--
+  <link rel="stylesheet" href="/stylesheets/foundation.css">
+  -->
+
+  <!-- Included CSS Files (Compressed) -->
+  <link rel="stylesheet" href="/stylesheets/foundation.min.css">
+
+  <script src="javascripts/modernizr.foundation.js"></script>
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,400italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="/stylesheets/general_foundicons.css">
+  <link rel="stylesheet" href="/stylesheets/general_enclosed_foundicons.css">
+  <link rel="stylesheet" href="/stylesheets/social_foundicons.css">
+  <link rel="stylesheet" href="/stylesheets/accessibility_foundicons.css">
+  <!--[if lt IE 8]>
+    <link href="/stylesheets/general_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="/stylesheets/general_enclosed_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="/stylesheets/social_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="/stylesheets/accessibility_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+  <![endif]-->
+  <link rel="stylesheet" href="/stylesheets/caslon_ampersand.css">
+  <link rel="stylesheet" href="/stylesheets/app.css">
+  <?= $document->head ?>
+</head>
+<body>
+  <!-- Header -->
+  <header>
+  <div class="row">
+    <div class="twelve columns">
+      <h1><a href="/"><i class="general foundicon-tools"></i></a><?= $document->title ?></h1>
+    </div>
+  </div>
+  <div class="row">
+   <div class="six columns">
+      <p><i class="accessibility foundicon-question"></i><br />
+      <?= $document->description ?>
+      </p>
+   </div>
+   <div class="six columns">
+      <p class="right"><a href="/"><i class="accessibility foundicon-braille"></i></a><br />
+      Tools — <em>Small utilities, gadgets and scripts to perform daily tasks.</em></p>
+    </div>
+  </div>
+  </header>
+
+  <!-- Body -->
+  <section id="content"><div class="row">
