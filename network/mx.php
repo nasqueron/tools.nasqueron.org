4234e0989f5259bba1ca4bda62768cd68b33bce8
Dereckson <dereckson@espace-win.org>
2013-02-13 01:32:37 +0100

    Improving network/mx tool

--- network/mx.php
+++ network/mx.php
@@ -1,21 +1,38 @@
 <?php
-	$getmx = '/home/dereckson/bin/getmx';
 	$mail = '';
 	if (isset($_REQUEST['mail'])) {
 		$mail = $_REQUEST['mail'];
-		$mx = `$getmx $mail`;
 	}
 ?>
+<h2>Search</h2>
+<form>
 <div class="row collapse">
   <div class="ten mobile-three columns">
-    <label for="mail">E-mail:</label> <input type="text" name="mail" id="mail" value="<?= $mail ?>" />
+    <input type="text" name="mail" id="mail" value="<?= $mail ?>" />
   </div>
   <div class="two mobile-one columns">
-    <a class="button expand postfix">Search</a>
+    <input type="submit" class="button expand postfix" value="Search" />
   </div>
 </div>
+</form>
 <?php
-	if ($mail) {
-		echo "<h2>Result</h2>\n<p>$mx</p>";
+if ($mail) {
+	echo "<h2>Result</h2>\n";
+	if (is_mail($mail)) {
+		//Gets domain name
+		$data = explode('@', $mail);
+		$domain = $data[1];
+
+		//Digs MX record
+		$getmx = '/home/dereckson/bin/getmx'; //TODO: move tools to a bin folder
+		$cmd = $getmx . ' ' . escapeshellarg($mail); //TODO: a nice function to automate this code
+		$mx = `$cmd`;
+
+		//Prints result
+		echo "<h3>SMTP server</h3>\n<p id='mx' class='result'>$mx</p>\n<h3>Determination method</h3>\n<p><strong>DNS lookup:</strong> $domain MX ?</p>";
+	} else {
+		echo "<h3>Error</h3>\n<p class='error'>$mail isn't a valid mail address.</p>";
+		echo "<h3>Improve this tool</h3><p>This code is open source, you can improve it. Fork it and add JS validation mail code. If the mail is wrong, apply the class .error to the input box.</p>";
 	}
+}
 ?>
