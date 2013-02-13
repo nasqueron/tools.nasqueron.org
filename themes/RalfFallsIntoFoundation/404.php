177d4d4c9da0350159aa650e68ab3c05d4783491
Dereckson <dereckson@espace-win.org>
2013-02-13 01:48:49 +0100

    404 page

---
+++ themes/RalfFallsIntoFoundation/404.php
@@ -0,0 +1,13 @@
+<div class="eight columns">
+	<h2>Something creative.</h2>
+	<p>This page should be print something creative about a 404 error but current doesn't.</p>
+	<p>That's not a nice behavior for such a page.</p>
+	<hr />
+	<p>So, let's state the fact:</p>
+	<h2>This is a 404 error.</h2>
+	<p>[ . ]</p>
+</div>
+<div class="four columns justify">
+	<h2>Mais où est donc cette créativité attendue ?</h2>
+	<p>Cette page devrait faire preuve de créativité et afficher quelque chose de spécial à propos des erreurs 404, avec une quelconque référence culturelle à un mème, mais ne le fais pas. On pourrait l'accuser d'anticonformisme en la matière.</p>
+</div>
