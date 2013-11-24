286bba48865f03b090781a492a7b4d8f4f5e8352
Dereckson <dereckson@espace-win.org>
2013-11-24 22:36:34 +0100

    Adding color/palette.php

---
+++ color/palette.php
@@ -0,0 +1,17 @@
+<?php
+$palettes = [
+    [
+        "title" => "Nike box",
+        "colors" => [
+            ["E33005", "Deep orange"],
+            ["333333", "Dark gray"],
+            ["F9F6EF", "Near white"],
+            ["000000", "Black"],
+            ["808080", "Middle gray"]
+        ]
+    ]
+];
+
+require('/home/dereckson.be/tools.dereckson.be/color/ase.php');
+$content = AdobeSwatchExchangeFile::getASEContent($palettes);
+file_put_contents("/tmp/nikebox.ase", $content);
