096ba4469d280bcb1e147ad37e31c97d046d0f4b
Dereckson <dereckson@espace-win.org>
2014-08-27 20:57:27 +0000

    Improving network/mx tool
    * It doesn't rely anymore to an external script, but uses PHP getmxrr function.
    * It silently accepts domain input, in addition to mail input.

--- network/mx.php
+++ network/mx.php
@@ -8,7 +8,7 @@
 <form>
 <div class="row collapse">
   <div class="ten mobile-three columns">
-    <input type="text" name="mail" id="mail" value="<?= $mail ?>" />
+    <input type="text" name="mail" id="mail" value="<?= $mail ?>" placeholder="Enter e-mail address here." />
   </div>
   <div class="two mobile-one columns">
     <input type="submit" class="button expand postfix" value="Search" />
@@ -16,20 +16,34 @@
 </div>
 </form>
 <?php
+function is_domain ($string) {
+	return preg_match($string, '/[a-zA-Z\d-]{,63}(\.[a-zA-Z\d-]{,63})*/');
+}
+
+function print_mx ($domain) {
+	if (getmxrr($domain, $mxhosts, $weights)) {
+		echo '<table id="mx" class="result">';
+		echo '<tr><th>Host</th><th>Weight</th></tr>';
+		$n = count($mxhosts);
+		for ($i = 0 ; $i < $n ; $i++) {
+			echo "<tr><td>$mxhosts[$i]</td><td>$weights[$i]</td></tr>";
+		}
+		echo '</table>';
+	} else {
+		echo '<p id="mx" class="result emptyresult">—</p>';
+	}
+	echo "<h3>Determination method</h3>\n<p><strong>DNS lookup:</strong> $domain MX ?</p>";
+}
+
 if ($mail) {
-	echo "<h2>Result</h2>\n";
+	echo "<h2>Result</h2>\n<h3>MX records</h3>";
 	if (is_mail($mail)) {
 		//Gets domain name
 		$data = explode('@', $mail);
-		$domain = $data[1];
-
-		//Digs MX record
-		$getmx = '/home/dereckson/bin/getmx'; //TODO: move tools to a bin folder
-		$cmd = $getmx . ' ' . escapeshellarg($mail); //TODO: a nice function to automate this code
-		$mx = `$cmd`;
-
-		//Prints result
-		echo "<h3>SMTP server</h3>\n<p id='mx' class='result'>$mx</p>\n<h3>Determination method</h3>\n<p><strong>DNS lookup:</strong> $domain MX ?</p>";
+		print_mx($data[1]);
+	} elseif (is_domain($mail)) {
+		//We silently accept direct domain entries given instead of a mail
+		print_mx($mail);
 	} else {
 		echo "<h3>Error</h3>\n<p class='error'>$mail isn't a valid mail address.</p>";
 		echo "<h3>Improve this tool</h3><p>This code is open source, you can improve it. Fork it and add JS validation mail code. If the mail is wrong, apply the class .error to the input box.</p>";
