f75c40a0c7d3cc9d0cf5ed81be6844e324122a05
Sébastien Santoro <dereckson@espace-win.org>
2015-06-15 22:56:48 +0000

    FilesListingController: new controller to list current files
    This controller is intended to be used in xmlHttpRequest entry points
    to return a JSON document with the URL to a list of files.
    
    For example, to return the URLs to every .txt files in current directory:
    
    ```
    $controller = new FilesListingController(
        get_directory(__DIR__),
        'txt'
    );
    
    $controller->query()->show();
    ```

---
+++ includes/controllers/FilesListingController.php
@@ -0,0 +1,61 @@
+<?php
+
+class FilesListingController {
+	///
+	/// Private members and properties
+	///
+
+	/**
+	 * QueryResult
+	 * @var array
+	 */
+	private $result;
+
+	/**
+	 * The directory to query
+	 * @var string
+	 */
+	public $directory;
+
+	/**
+	 * The extension of files to query
+	 * @var string
+	 */
+	public $extension;
+
+	///
+	/// Construtor
+	///
+	public function __construct ($directory, $extension) {
+		$this->directory = $directory;
+		$this->extension = $extension;
+	}
+
+	///
+	/// Public methods
+	///
+
+	/**
+     * Queries the files
+	 *
+	 * @return FilesListing the current class instance
+	 */
+	public function query () {
+		$files = get_files($this->directory, $this->extension);
+
+		//Prepends URL directory
+		array_walk($files, function (&$file) {
+			$file = get_url_for($this->directory, $file);
+		});
+
+		$this->result = $files;
+		return $this;
+	}
+
+	/**
+	 * Outputs the result
+	 */
+	public function show () {
+		echo json_encode($this->result);
+	}
+}
