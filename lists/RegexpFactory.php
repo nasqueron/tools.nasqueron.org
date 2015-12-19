804ab6fa455e5867d46c7c09d1ac2cdd31a3353a
Sébastien Santoro <dereckson@espace-win.org>
2015-12-19 17:30:53 +0000

    list/replace: multi regexp support
    Summary:
    The fields expression and replacement are now handled a list of
    expressions/replacements, delimited by new lines.
    
    This allow to perform several regexp replacements in one operation.
    
    This change affects only back-end code. A temptative UI change lives
    at D107 but doesn't give satisfaction, as to change between a input
    box (to get history) and a textarea (to encode seval expressions) is
    rather tricky. A proper combobox/textarea control is needed. If the
    UI uses React, it would also be easier to implement his, as we have
    the state of the widget distinct of the representation.
    
    Test Plan: live test at D107
    
    Reviewers: dereckson
    
    Maniphest Tasks: T626
    
    Differential Revision: http://devcentral.nasqueron.org/D110

--- lists/RegexpFactory.php
+++ lists/RegexpFactory.php
@@ -5,10 +5,16 @@
  */
 class RegexpFactory {
     /**
-     * The regular expression
-     * @var string
+     * The regular expressions
+     * @var string[]
      */
-    public $expression;
+    public $expressions;
+
+    /**
+     * The replacement expressions
+     * @var string[]
+     */
+    public $replaceExpressions;
 
     /**
      * The last error
@@ -17,49 +23,91 @@ class RegexpFactory {
     public $lastError;
 
     /**
+     * Regular expression to delimit regexps/replaces
+     */
+    const DELIMITER = '/\r\n|\n|\r/';
+
+    /**
      * Initializes a new instance of the RegexpFactory object.
      *
-     * @param string The regular expression
+     * @param string $rexpressions The regular expression
+     * @param string $replaceExpressions The format of the result string
      */
-    function __construct ($expression) {
-        $this->expression = $expression;
+     function __construct ($expressions, $replaceExpressions) {
+        $this->expressions = preg_split(static::DELIMITER, $expressions);
+        $this->replaceExpressions = preg_split(static::DELIMITER, $replaceExpressions);
+
+        if (count($this->expressions) != count($this->replaceExpressions)) {
+            throw new Exception("The number of expressions and replacements should match.");
+        }
     }
 
     /**
      * Replaces an expression using regexp, a similar way Apache mod_rewrite and Nginx wreplace do.
      *
      * @param string $haystack The expression to perform a replacement on
-     * @param string $replaceExpression The format of the result string
      * @return string The replaced string
      */
-    function replace ($haystack, $replaceExpression) {
-        return preg_replace($this->expression, $replaceExpression, $haystack);
+    function replace ($haystack) {
+        $text = $haystack;
+        for ($i = 0 ; $i < count($this->expressions) ; $i++) {
+            $expression = $this->expressions[$i];
+            $replaceExpression = $this->replaceExpressions[$i];
+            $text = preg_replace($expression, $replaceExpression, $text);
+        }
+        return $text;
     }
 
     /**
-     * Encloses the regular expression with delimiters.
+     * Adds delimiters to each regular expression.
      */
-    function addDelimiters () {
+    public function addDelimiters () {
+        array_walk($this->expressions, function (&$item) {
+            $item = RegexpFactory::addDelimitersToExpression($item);
+        });
+    }
+
+    /**
+     * Encloses the specified regular expression with delimiters.
+     *
+     * @param string $expression The expression to delimit
+     * @return string The expression with delimiters
+     */
+    public static function addDelimitersToExpression ($expression) {
         $delimiters = ['/', '@', '#', '~', '+', '%', '♦', 'µ', '±', '☞'];
         //TODO: check if it's okay to use UTF-8 characters as delimiters
         foreach ($delimiters as $delimiter) {
-            if (strpos($this->expression, $delimiter) === false) {
-                $this->expression = $delimiter . $this->expression . $delimiter;
-                return;
+            if (strpos($expression, $delimiter) === false) {
+                return $delimiter . $expression . $delimiter;
             }
         }
-        throw new Exception("Can't delimite regexp $this->expression");
+        throw new Exception("Can't delimite regexp $expression");
+    }
+
+    /**
+     * Determines if the specified expression block is valid.
+     *
+     * @return bool true if each expression is valid; otherwise, false.
+     */
+    public function isValid () {
+        foreach ($this->expressions as $expression) {
+            if (!$this->isValidExpression($expression)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
      * Determines if the specified expression is valid.
      *
+     * @param string the regexp to test
      * @return bool true if the expression is valid; otherwise, false.
      */
-    public function isValid () {
+    public function isValidExpression ($expression) {
         $this->lastError = '';
         set_error_handler('self::handleErrors');
-        $result = preg_match($this->expression, null);
+        $result = preg_match($expression, null);
         restore_error_handler();
         if ($this->lastError === '' && $result === false) {
             $this->lastError = self::getPCREError();
