83c0e883d0937c817b53ce8b11c6e04a9cc610e5
Sébastien Santoro <dereckson@espace-win.org>
2016-01-29 01:54:45 +0000

    Remove Netfront UA kludge
    Summary:
    A kludge allowed to print a tool on an old text phone without
    header nor footer. This kludge isn't needed anymore.
    
    Fixes T700.
    
    Test Plan: /rnd/
    
    Reviewers: dereckson
    
    Maniphest Tasks: T700
    
    Differential Revision: http://devcentral.nasqueron.org/D267

--- index.php
+++ index.php
@@ -39,22 +39,6 @@ $CurrentUser = $Session->get_logged_user();
 /// Serves the requested page
 ///
 
-/**
- * Forces bare display for broken mobiles
- *
- * @return true if header and footer must be skipped ; otherwise, false.
- */
-function force_bare_display () {
-	if (strpos($_SERVER["HTTP_USER_AGENT"], " NetFront/") !== false) {
-		return true;
-	}
-	return false;
-}
-
 $url = get_current_url();
 $document = new Document($url);
-if (force_bare_display()) {
-	$document->noheader = true;
-	$document->nofooter = true;
-}
 $document->render();
